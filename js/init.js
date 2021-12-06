57ad4ca9464e4ec26c89a5e47e27adc875006ba9
proyectofinal9999 <proyectofinal9999@gmail.com>
2021-12-06 15:18:30 -0600

    Configuracion Init

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyAknrvw3brgo7qxEyMk3jzsE02CyJKWo5c",
-    authDomain: "fbauten.firebaseapp.com",
-    projectId: "fbauten",
-    storageBucket: "fbauten.appspot.com",
-    messagingSenderId: "902838344264",
-    appId: "1:902838344264:web:221d59cd0ba838db98567d",
-    measurementId: "G-52E0LME3E0"
+  apiKey: "AIzaSyDZw19jRW3yZxQPEimpj9_Rt3XbuAULlXQ",
+  authDomain: "proyectofinal-9bb56.firebaseapp.com",
+  projectId: "proyectofinal-9bb56",
+  storageBucket: "proyectofinal-9bb56.appspot.com",
+  messagingSenderId: "92968985750",
+  appId: "1:92968985750:web:d16acac178ea2a58c30eb7",
+  measurementId: "G-8E1XKYGYG1"
   });
